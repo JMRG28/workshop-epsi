4428f4149bc5f7d2f4097f67baab6a86370dae67
ramzizer <ramzizer@hotmail.com>
2021-09-28 13:34:51 +0200

    UserModif

--- migrations/Version20210928112853.php
+++ migrations/Version20210928112853.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210928070842 extends AbstractMigration
+final class Version20210928112853 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,20 +20,19 @@ final class Version20210928070842 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE quizz (id INT AUTO_INCREMENT NOT NULL, score INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE quizz_question (quizz_id INT NOT NULL, question_id INT NOT NULL, INDEX IDX_3723B55CBA934BCD (quizz_id), INDEX IDX_3723B55C1E27F6BF (question_id), PRIMARY KEY(quizz_id, question_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user_quizz (user_id INT NOT NULL, quizz_id INT NOT NULL, INDEX IDX_9EB56C65A76ED395 (user_id), INDEX IDX_9EB56C65BA934BCD (quizz_id), PRIMARY KEY(user_id, quizz_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE quizz_question ADD CONSTRAINT FK_3723B55CBA934BCD FOREIGN KEY (quizz_id) REFERENCES quizz (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE quizz_question ADD CONSTRAINT FK_3723B55C1E27F6BF FOREIGN KEY (question_id) REFERENCES question (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE user_quizz ADD CONSTRAINT FK_9EB56C65A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE user_quizz ADD CONSTRAINT FK_9EB56C65BA934BCD FOREIGN KEY (quizz_id) REFERENCES quizz (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE question ADD cour_id INT DEFAULT NULL, CHANGE enonce enonce TINYTEXT NOT NULL');
+        $this->addSql('ALTER TABLE question ADD cour_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE question ADD CONSTRAINT FK_B6F7494EB7942F03 FOREIGN KEY (cour_id) REFERENCES cour (id)');
         $this->addSql('CREATE INDEX IDX_B6F7494EB7942F03 ON question (cour_id)');
         $this->addSql('ALTER TABLE reponse ADD question_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE reponse ADD CONSTRAINT FK_5FB6DEC71E27F6BF FOREIGN KEY (question_id) REFERENCES question (id)');
         $this->addSql('CREATE INDEX IDX_5FB6DEC71E27F6BF ON reponse (question_id)');
-        $this->addSql('ALTER TABLE user ADD entreprise_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD entreprise_id INT DEFAULT NULL, ADD nom VARCHAR(255) DEFAULT NULL, ADD prenom VARCHAR(255) DEFAULT NULL, ADD image VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D649A4AEAFEA FOREIGN KEY (entreprise_id) REFERENCES entreprise (id)');
         $this->addSql('CREATE INDEX IDX_8D93D649A4AEAFEA ON user (entreprise_id)');
     }
@@ -41,19 +40,16 @@ final class Version20210928070842 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE quizz_question DROP FOREIGN KEY FK_3723B55CBA934BCD');
-        $this->addSql('ALTER TABLE user_quizz DROP FOREIGN KEY FK_9EB56C65BA934BCD');
-        $this->addSql('DROP TABLE quizz');
         $this->addSql('DROP TABLE quizz_question');
         $this->addSql('DROP TABLE user_quizz');
         $this->addSql('ALTER TABLE question DROP FOREIGN KEY FK_B6F7494EB7942F03');
         $this->addSql('DROP INDEX IDX_B6F7494EB7942F03 ON question');
-        $this->addSql('ALTER TABLE question DROP cour_id, CHANGE enonce enonce VARCHAR(500) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE question DROP cour_id');
         $this->addSql('ALTER TABLE reponse DROP FOREIGN KEY FK_5FB6DEC71E27F6BF');
         $this->addSql('DROP INDEX IDX_5FB6DEC71E27F6BF ON reponse');
         $this->addSql('ALTER TABLE reponse DROP question_id');
         $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D649A4AEAFEA');
         $this->addSql('DROP INDEX IDX_8D93D649A4AEAFEA ON user');
-        $this->addSql('ALTER TABLE user DROP entreprise_id');
+        $this->addSql('ALTER TABLE user DROP entreprise_id, DROP nom, DROP prenom, DROP image');
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -47,6 +47,21 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     private $entreprise;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $prenom;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $image;
+
     public function __construct()
     {
         $this->quizz = new ArrayCollection();
@@ -173,6 +188,42 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setEntreprise(?entreprise $entreprise): self
     {
         $this->entreprise = $entreprise;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(?string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(?string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
 
         return $this;
     }
