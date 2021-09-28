c1a9efa987312fd856a36431f471ed5315d70f02
Isil <yoann.lecanu@gmail.com>
2021-09-28 14:22:52 +0200

    first commit yoann

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -13,6 +13,25 @@ class MainController extends AbstractController
     {
             return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+            
+        ]);
+    }
+
+    #[Route('/tests', name: 'PageTest')]
+    public function PageTest(): Response
+    {
+            return $this->render('main/pageTest.html.twig', [
+            'controller_name' => 'MainController',
+            
+        ]);
+    }
+
+    #[Route('/resultat', name: 'Resultat')]
+    public function Resultats(): Response
+    {
+            return $this->render('main/resultats.html.twig', [
+            'controller_name' => 'MainController',
+            
         ]);
     }
 
