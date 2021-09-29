73670b3434572706bdffa817d2467e18bae67094
Jean-Maurice <jeanmaurice.raboude@yahoo.com>
2021-09-29 14:50:01 +0200

    fixed fixtures

--- src/DataFixtures/QuestionFixtures.php
+++ src/DataFixtures/QuestionFixtures.php
@@ -36,56 +36,68 @@ class QuestionFixtures extends Fixture
         Il est en effet rare (voire impossible) qu'une banque vous demande des renseignements aussi importants par un simple courrier électronique. Dans le doute contactez directement votre agence par téléphone !
         Assurez-vous, lorsque vous saisissez des informations sensibles, que le navigateur est en mode sécurisé, c'est-à-dire que l'adresse dans la barre du navigateur commence par https et qu'un petit cadenas est affiché dans la barre d'état au bas de votre navigateur, et que le domaine du site dans l'adresse correspond bien à celui annoncé (gare à l'orthographe du domaine) !
         ");
+        $manager->persist($c1);
         $q1 = new Question(); 
         $q1->setEnonce("Vous recevez un mail provenant de SFR")
         ->setTheme("Mail")
         ->setCour($c1)
         ->setDifficulte("Facile");
+        $manager->persist($q1);
         $q2 = new Question(); 
         $q2->setEnonce("Vous recevez un mail de votre banque Banque Populaire <noreply@banquepopulaire.frx> que faîtes-vous ?")
         ->setTheme("Mail")
         ->setCour($c1)
         ->setDifficulte("Facile");
+        $manager->persist($q2);
         $q3 = new Question(); 
         $q3->setEnonce("Vous êtes raccordé à une agence pour votre habitation et vous recevez une pièce jointe provenant du mail de l'agence présente sur le contrat : immobilierdelaplage@gmail.com, vous l'ouvrez ? ")
         ->setTheme("Mail")
         ->setCour($c1)
         ->setDifficulte("Facile");
+        $manager->persist($q3);
         $q4 = new Question(); 
         $q4->setEnonce("Vous savez que vous devez recevoir un remboursement des impôts concernant une erreur dans votre déclaration ,cliquez-vous sur le lien reçu depuis ce mail ?")
         ->setTheme("Mail")
         ->setCour($c1)
         ->setDifficulte("Moyen");
+        $manager->persist($q4);
         $q5 = new Question(); 
         $q5->setEnonce("Que faîtes vous ?")
         ->setTheme("Mail")
         ->setCour($c1)
         ->setDifficulte("Moyen");
+        $manager->persist($q5);
         $q6 = new Question(); 
         $q6->setEnonce("Ce mail en provenance de SFR vous semble-t-il normal ?")
         ->setTheme("Mail")
         ->setCour($c1)
         ->setDifficulte("Moyen");
+        $manager->persist($q6);
         $q7 = new Question(); 
         $q7->setEnonce("Ce mail en provenance de Chronopost vous semble-t-il normal ?")
         ->setTheme("Mail")
         ->setCour($c1)
         ->setDifficulte("Moyen");
+        $manager->persist($q7);
         $q8= new Question(); 
         $q8->setEnonce("Qu'est-ce que le phishing")
         ->setTheme("Mail")
         ->setCour($c1)
         ->setDifficulte("Difficile");
+        $manager->persist($q8);
         $q9 = new Question(); 
         $q9->setEnonce("Vous êtes dans l'attente de réception d'un mail d'une personne de votre entourage, enregistrez vous la pièce jointe ci-dessus présente dans le mail ?")
         ->setCour($c1)
         ->setTheme("Mail")
         ->setDifficulte("Difficile");
+        $manager->persist($q9);
+        ;
         $q10 = new Question(); 
         $q10->setEnonce("Jsp")
         ->setDifficulte("Difficile")
         ->setCour($c1)
         ->setTheme("Mail");
+        $manager->persist($q10);
         
         $manager->flush();
     }
